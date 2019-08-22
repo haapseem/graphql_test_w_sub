6da4a88fd9027619711098ee061ea77078ccb0e9
Eemil Haapsaari <eemil.haapsaari@vismappg.fi>
2019-08-22 14:57:32 +0300

    forget old observables

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ from flask_cors import CORS
 from graphql.execution.base import ExecutionResult
 
 logging.getLogger().setLevel(logging.DEBUG)
+connections = [] # socket io connections
+
+def in_con(sid):
+    return sid in connections
+
 #
 # schema
 #
@@ -31,11 +36,10 @@ class Query(ObjectType):
         return 'See ya!'
 
 class Subscription(ObjectType):
-    hello = String()
-    goodbye = String()
+    time = String(sid = String(default_value=""))
 
-    def resolve_hello(root, info):
-        return Observable.interval(1000).map(lambda x: str(datetime.datetime.now()))
+    def resolve_time(root, info, sid):
+        return Observable.interval(100).map(lambda x: str(datetime.datetime.now())).take_while(lambda x: in_con(sid))
 
     def resolve_goodbye(root, info):
         return 'See ya!'
@@ -58,16 +62,29 @@ def home():
 def graphql_query():
     q = request.args.get('query')
     q = schema.execute(q, allow_subscriptions=True)
+
     if type(q) == ExecutionResult:
         q=json.dumps(q.data)
         return q
     else:
-        q.subscribe(lambda x: sio.emit('sub',x.data))
-    return "a"
+        sid = request.args.get('sid')
+        print("here", sid)
+        q.subscribe(lambda x: sio.emit('sub',x.data,room=sid))
+    return " "
 
+@sio.on("connect")
+def con(sid, env):
+    connections.append(sid)
+    print("connections", connections)
+
+@sio.on("disconnect")
+def discon(sid):
+    connections.remove(sid)
+    print(connections)
 
 CORS(app)
 app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
 if __name__ == '__main__':
-    app.run()
+    print("hello")
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app, debug=True)
